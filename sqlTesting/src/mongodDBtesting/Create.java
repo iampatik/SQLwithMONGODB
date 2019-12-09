d31bb56bc51dbe00c4affbd019fb59e08ec7f89d
Lucifer Morningstar <star808fer@gmail.com>
2019-12-09 17:09:54 +0800

    near to finish

--- sqlTesting/src/mongodDBtesting/Create.java
+++ sqlTesting/src/mongodDBtesting/Create.java
@@ -1,25 +1,100 @@
 package mongodDBtesting;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.Cursor;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import java.text.SimpleDateFormat;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import com.mongodb.client.MongoDatabase; 
-import com.mongodb.MongoClient; 
-import com.mongodb.MongoCredential;  
+public class Create {
 
-public class Create { 
-   
-   public static void main( String args[] ) {  
-      
-      // Creating a Mongo client 
-      MongoClient mongo = new MongoClient( "localhost" , 27017 ); 
-   
-      // Creating Credentials 
-      MongoCredential credential; 
-      credential = MongoCredential.createCredential("sampleUser", "myDb", 
-         "password".toCharArray()); 
-      System.out.println("Connected to the database successfully");  
-      
-      // Accessing the database 
-      MongoDatabase database = mongo.getDatabase("myDb"); 
-      System.out.println("Credentials ::"+ credential);     
-   } 
+    public void create() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	Date date = new Date();
+	System.out.println("Time started: "+formatter.format(date));
+        Instant fore = Instant.now();
+        try {
+            Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+            mongoLogger.setLevel(Level.SEVERE);
+            MongoClient mongoClient = new MongoClient("localhost", 27017);
+            DB db = mongoClient.getDB("test");
+            DBCollection collection = db.getCollection("sqltesting");
+            for (int i = 1; i < 1001; i++) {
+                BasicDBObject add1000 = new BasicDBObject("col1", i)
+                        .append("col2", i + 1)
+                        .append("col3", i + 2)
+                        .append("col4", i + 3)
+                        .append("col5", i + 4);
+                collection.insert(add1000);
+                System.out.println("Inserted!");
+            }
+        } catch (SecurityException e) {
+            System.out.println("Error is: " + e);
+        }
+        Instant ter = Instant.now();
+        Duration duration = Duration.between(fore, ter);
+        System.out.println("Time ended: " + formatter.format(date));
+        System.out.println("Duration: " + duration.getSeconds()+ " second/s");
+    }
+
+    public void delete() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	Date date = new Date();
+	System.out.println("Time started: "+formatter.format(date));
+        Instant fore = Instant.now();
+        try {
+            Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+            mongoLogger.setLevel(Level.SEVERE);
+            MongoClient mongoClient = new MongoClient("localhost", 27017);
+            DB db = mongoClient.getDB("test");
+            System.out.println("Connected to database!");
+            DBCollection collection = db.getCollection("sqltesting");
+
+            for (int i = 1; i < 1001; i++) {
+                BasicDBObject remove1000 = new BasicDBObject("col1", i);
+                collection.remove(remove1000);
+                System.out.println("Suuccessfully deleted!");
+            }
+        } catch (SecurityException e) {
+            System.out.println("Error connecting to database!");
+        }
+        Instant ter = Instant.now();
+        Duration duration = Duration.between(fore, ter);
+        System.out.println("Time ended: "+formatter.format(date));
+        System.out.println("Duration: " + duration.getSeconds()+" second/s");
+    }
+
+    public void get() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	Date date = new Date();
+	System.out.println("Time started: "+formatter.format(date));
+        Instant fore = Instant.now();
+        try {
+            Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+            mongoLogger.setLevel(Level.SEVERE);
+            MongoClient mongoClient = new MongoClient("localhost", 27017);
+            DB db = mongoClient.getDB("test");
+            System.out.println("Connected to database!");
+            DBCollection collection = db.getCollection("sqltesting");
+            Cursor cursor = collection.find();
+            while(cursor.hasNext()){
+                System.out.println(cursor.next());
+            }
+        } catch (SecurityException e) {
+            System.out.println("Error connecting to database!");
+        }
+        Instant ter = Instant.now();
+        Duration duration = Duration.between(fore, ter);
+        System.out.println("Time ended: "+formatter.format(date));
+        System.out.println("Duration: " + duration.getSeconds()+" second/s");
+
+    }
+
 }
